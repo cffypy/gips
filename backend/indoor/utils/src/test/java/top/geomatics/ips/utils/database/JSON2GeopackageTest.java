ffca41502073d9e1f04344143f61c7b7acd201c4
geodyj <whudyj2010@hotmail.com>
2019-09-23 18:41:23 +0800

    增加指纹库json转geopackage功能

--- backend/indoor/utils/src/test/java/top/geomatics/ips/utils/database/JSON2GeopackageTest.java
+++ backend/indoor/utils/src/test/java/top/geomatics/ips/utils/database/JSON2GeopackageTest.java
@@ -1,14 +1,18 @@
 package top.geomatics.ips.utils.database;
 
+import java.io.File;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class JSON2GeopackageTest {
 	private JSON2Geopackage j2p;
-	
+	private String filePath = "D:\\ips\\data\\database\\shilintong";
+
 	@Before
 	public void setup() {
-		j2p = new JSON2Geopackage("D:\\ips\\data\\database\\json\\WifiFPfloor.json","");
+		String gpkgFN = filePath + File.separator + "shilintong.gpkg";
+		j2p = new JSON2Geopackage(filePath, gpkgFN);
 	}
 
 	@Test
