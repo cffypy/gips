953dfd367bde79a7d8e75761ca81c38db9737908
geodyj <whudyj2010@hotmail.com>
2019-12-05 11:15:20 +0800

    Update ImageDataReader.java

--- backend/indoor/utils/src/main/java/top/geomatics/ips/utils/image/ImageDataReader.java
+++ backend/indoor/utils/src/main/java/top/geomatics/ips/utils/image/ImageDataReader.java
@@ -14,10 +14,16 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bytedeco.javacpp.opencv_core.KeyPoint;
-import org.bytedeco.javacpp.opencv_core.Mat;
-import org.bytedeco.javacpp.opencv_core.Point2f;
-import org.bytedeco.javacpp.opencv_core.Point3d;
+//import org.bytedeco.javacpp.opencv_core.KeyPoint;
+//import org.bytedeco.javacpp.opencv_core.Mat;
+//import org.bytedeco.javacpp.opencv_core.Point2f;
+//import org.bytedeco.javacpp.opencv_core.Point3d;
+
+import org.opencv.core.Mat;
+import org.opencv.core.KeyPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Point3;
+import org.opencv.core.Core;
 
 import com.alibaba.fastjson.JSON;
 
@@ -31,6 +37,8 @@ public class ImageDataReader {
 	private List<Data> objs = new ArrayList<Data>();
 
 	public void loadOBJ(String filename, String wr) {
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
 		File file = new File(filename);
 		if (!file.exists()) {
@@ -47,20 +55,20 @@ public class ImageDataReader {
 
 			for (int i = 0; i < cnt; i++) {
 				Data t = new Data();
+				//读取特征点id
 				t.id = Integer.reverseBytes(is.readInt());
 				writeLine(writer, "id: " + t.id);
 
+				//读取三维坐标
 				double x = Double.longBitsToDouble(Long.reverseBytes(is.readLong()));
 				double y = Double.longBitsToDouble(Long.reverseBytes(is.readLong()));
 				double z = Double.longBitsToDouble(Long.reverseBytes(is.readLong()));
-				System.out.println("x:" + x + " y: " + y + " z: " + z);
+				//System.out.println("x:" + x + " y: " + y + " z: " + z);
 				writeLine(writer, "x:" + x + " y: " + y + " z: " + z);
 
-				t.pt = new Point3d();
-				t.pt.put(x);
-				t.pt.put(y);
-				t.pt.put(z);
+				t.pt3 = new Point3(x,y,z);
 
+				//读取特征点个数size
 				int sucnt = Integer.reverseBytes(is.readInt());
 				writeLine(writer, "size: " + sucnt);
 				for (int j = 0; j < sucnt; j++) {
@@ -79,14 +87,13 @@ public class ImageDataReader {
 					int oct = Integer.reverseBytes(is.readInt());
 					int cls_id = Integer.reverseBytes(is.readInt());
 
+					//写下每一个特征点的图像坐标、角度、层次等信息
 					String c = String.format("KeyPoint: {px:%f py:%f sz:%f angle: %f response:%f oct:%d cls_id:%d }",
 							px, py, sz, angle, response, oct, cls_id);
 
 					writeLine(writer, c);
-
-					Point2f pf = new Point2f(px, py);
-
-					key.pt = new KeyPoint(pf, sz, angle, response, oct, cls_id);
+					//为data属性赋值
+					key.kpt = new KeyPoint(px,py, sz, angle, response, oct, cls_id);
 					t.marks.add(key);
 				}
 
@@ -94,18 +101,32 @@ public class ImageDataReader {
 				int cols = Integer.reverseBytes(is.readInt());
 				int type_ = Integer.reverseBytes(is.readInt());
 
+				//该特征描述子的结构
 				String sc = String.format("Mat: {rows:%d cols:%d type:%d }", rows, cols, type_);
 
 				writeLine(writer, sc);
 
-				t.des = new Mat(rows, cols, type_);
-				int total_ = rows * t.des.step(0);
-				byte b[] = new byte[total_];
-				int sss = is.read(b);
+				//
+				t.des = new Mat(rows, cols , type_);
+				long total_ = rows * t.des.step1(0);
+				//byte b[] = new byte[total_];
+				//int sss = is.read(b);
+
+
+				//读取64维特征向量的每一维的数字
+				for(int m = 0; m < cols; m++){
+					float descriptor_m = Float.intBitsToFloat(Integer.reverseBytes(is.readInt()));
+					t.des.put(0,m,descriptor_m);
+					//String ss = String.format("descriptor:%f",descriptor_m);
+					//writeLine(writer, ss);
+
+				}
+
 				int cx = is.readUnsignedByte();
 				int cy = is.readUnsignedByte();
 				int cz = is.readUnsignedByte();
 
+				//写下特征点颜色
 				sc = String.format("colors: {cx:%d cy:%d cz:%d }", cx, cy, cz);
 
 				writeLine(writer, sc);
@@ -114,9 +135,9 @@ public class ImageDataReader {
 				t.colors[0] = cx;
 				t.colors[1] = cy;
 				t.colors[2] = cz;
-
+                
 				objs.add(t);
-			}
+			}//读取完一个Data项
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -126,6 +147,18 @@ public class ImageDataReader {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+
+	}
+
+	//获取特征描述子及三维点集
+	public void loadDes(Mat desc, List<Point3> pt3List){
+
+		int featureNum = objs.size();
+
+		for (Data it:objs){
+			desc.push_back(it.des);
+			pt3List.add(it.pt3);
 		}
 
 	}
@@ -162,6 +195,7 @@ public class ImageDataReader {
 		return writer;
 	}
 
+	//数据项，对应一个空间特征点，包含id,对应的图像特征点，描述子，三维坐标，色彩信息。
 	public class Data {
 
 		public Data() {
@@ -171,7 +205,7 @@ public class ImageDataReader {
 		int id = 0;
 		List<IMG_KEY> marks;
 		Mat des;// 描述子
-		Point3d pt;
+		Point3 pt3;
 		int[] colors;
 
 	}
@@ -179,6 +213,6 @@ public class ImageDataReader {
 	public class IMG_KEY {
 		int IMG_ID;
 		int KEY_ID;
-		KeyPoint pt;
+		KeyPoint kpt;
 	}
 }
