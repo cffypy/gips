9c4a8e94e7e900708c14487623c9e7a2be47e946
whu_gj <30277881+Grace1126@users.noreply.github.com>
2019-12-06 10:50:38 +0800

    上传jar包

--- backend/indoor/utils/src/test/java/top/geomatics/ips/algorithms/image/ImageDataReaderTest.java
+++ backend/indoor/utils/src/test/java/top/geomatics/ips/algorithms/image/ImageDataReaderTest.java
@@ -31,9 +31,9 @@ public class ImageDataReaderTest {
         List<Point3> point3s = new ArrayList<Point3>();
         String pathString = "E:\\Projects\\诗琳通视觉定位数据";
         String rf = pathString + File.separator + "extractorType-OBJ-212.dat";
-        String wf = pathString + File.separator + "extractorType-OBJ-212111.json";
+        String wf = pathString + File.separator + "extractorType-OBJ-212545.json";
         ir.loadOBJ(rf,wf);
-	    ir.loadDes(descriptors, point3s);
+//	    ir.loadDes(descriptors, point3s);
 
     }
 
