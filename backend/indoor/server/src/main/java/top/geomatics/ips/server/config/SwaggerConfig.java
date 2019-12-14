3162eca5ae53e8317d144f837204c90e36026076
geoClound <whudyj2010@hotmail.com>
2019-12-14 08:47:59 +0800

    add configure

--- backend/indoor/server/src/main/java/top/geomatics/ips/server/config/SwaggerConfig.java
+++ backend/indoor/server/src/main/java/top/geomatics/ips/server/config/SwaggerConfig.java
@@ -1,11 +1,14 @@
-package top.geomatics.ips.server;
+package top.geomatics.ips.server.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -17,18 +20,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+//@EnableWebMvc
+//@ComponentScan(basePackages = {"top.geomatics.ips.server.controller"})
 public class SwaggerConfig {
 
 	@Bean
 	public Docket controllerApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(new ApiInfoBuilder()
-						.title("室内定位接口文档")
-						.version("版本号:1.0")
-						.build())
+				.apiInfo(apiInfo())
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("top.geomatics.ips.server.controller"))
 				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo(){
+		Contact contact = new Contact("dyj","http://www.geomatics.top","397095092@qq.com");
+		return new ApiInfoBuilder()
+				.title("室内定位Rest服务接口文档")
+				.version("1.0.0")
+				.description("API接口")
+				.contact(contact)
 				.build();
 	}
 
